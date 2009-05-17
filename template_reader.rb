db4a81a756abff45368c0cd3e12ab2c0075fcfbf
Lin Jen-Shin <godfat@godfat.org>
2009-05-18 03:56:26 +0800

    complete implement

--- template_reader.rb
+++ template_reader.rb
@@ -1,11 +1,15 @@
 #encoding: utf-8
 
+# spec from: http://guildwars.wikia.com/wiki/Skill_template_format
+
 module GW
   class TemplateReader
     Template = {14 => 'Skills'}
     Profession = %w[None Warrior Ranger Monk Necromancer Mesmer Elementalist Assassin Ritualist Paragon Dervish]
+    Attributes = File.read(File.join(File.dirname(__FILE__), 'code_attributes.txt')).split("\n")
+    Skills     = File.read(File.join(File.dirname(__FILE__), 'code_skills.txt'    )).split("\n")
 
-    attr_reader(:code, :template, :version, :primary, :secondary)
+    attr_reader(:code, :template, :version, :primary, :secondary, :attributes, :skills)
 
     def initialize code
       @code = code.dup.freeze
@@ -13,15 +17,38 @@ module GW
 
       @template  = extract!(4)
       @version   = extract!(4)
-      bits       = extract!(2) * 2 + 4
-      @primary   = extract!(bits)
-      @secondary = extract!(bits)
+      bits_pro   = extract!(2) * 2 + 4
+      @primary   = extract!(bits_pro)
+      @secondary = extract!(bits_pro)
+
+      attrs      = extract!(4)
+      bits_att   = extract!(4) + 4
+      @attributes = []
+      attrs.times{
+        @attributes << [extract!(bits_att), extract!(4)]
+      }
+
+      bits_ski   = extract!(4) + 8
+      @skills = []
+      8.times{
+        @skills << extract!(bits_ski)
+      }
     end
 
     def display
       puts("  Template: #{Template[template]}")
       puts("   Version: #{version}")
       puts("Profession: #{Profession[primary]} / #{Profession[secondary]}")
+      puts
+      puts("Attributes:")
+      @attributes.each{ |attribute|
+        puts("%20s %2d" % [Attributes[attribute.first], attribute.last])
+      }
+      puts
+      puts("    Skills:")
+      8.times{ |i|
+        puts("%23s" % Skills[@skills[i]][5..-1])
+      }
     end
 
     private
